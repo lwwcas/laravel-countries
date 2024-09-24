3473d167ecae9a8931d101df285f84e1a606aabb
Lucas Duarte <lwwcas@gmail.com>
2024-09-24 01:48:26 -0300

    test: start and config php pest tests

---
+++ tests/Pest.php
@@ -0,0 +1,46 @@
+<?php
+
+use Lwwcas\LaravelCountries\Tests\TestCase;
+
+/*
+|--------------------------------------------------------------------------
+| Test Case
+|--------------------------------------------------------------------------
+|
+| The closure you provide to your test functions is always bound to a specific PHPUnit test
+| case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
+| need to change it using the "pest()" function to bind a different classes or traits.
+|
+*/
+
+uses(TestCase::class)->in(__DIR__);
+
+beforeEach(function () {
+    $this->setUp();
+});
+
+/*
+|--------------------------------------------------------------------------
+| Expectations
+|--------------------------------------------------------------------------
+|
+| When you're writing tests, you often need to check that values meet certain conditions. The
+| "expect()" function gives you access to a set of "expectations" methods that you can use
+| to assert different things. Of course, you may extend the Expectation API at any time.
+|
+*/
+
+test('confirm environment is set to testing', function () {
+    expect(config('app.env'))->toBe('testing');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Functions
+|--------------------------------------------------------------------------
+|
+| While Pest is very powerful out-of-the-box, you may have some testing code specific to your
+| project that you don't want to repeat in every file. Here you can also expose helpers as
+| global functions to help you to reduce the number of lines of code in your test files.
+|
+*/
