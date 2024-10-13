b61844a38fc010866fed5eb3eae272ade34e1d2a
Lucas Duarte <lwwcas@gmail.com>
2024-10-13 11:44:10 -0300

    feat: new list methods

--- src/facades/FlagEmoji.php
+++ src/facades/FlagEmoji.php
@@ -2,6 +2,7 @@
 
 namespace Lwwcas\LaravelCountries\Facades;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Lwwcas\LaravelCountries\Models\Concerns\HasFlagEmojiGetters;
 
@@ -9,42 +10,55 @@ final class FlagEmoji
 {
     use HasFlagEmojiGetters;
 
-    protected ?array $flag_emoji = null;
-    protected ?array $on_data = null;
+    protected $flag_emoji = null;
+    protected $on_data = null;
+    protected $key = null;
 
     /**
      * FlagEmoji constructor.
      *
      * @param string $attributes
      */
-    public function __construct(protected Collection $attributes)
+    public function __construct(protected Collection|array $attributes)
     {
         if ($attributes instanceof Collection) {
-            foreach ($attributes as $item) {
-                $this->on_data[] = $item;
+            foreach ($attributes as $index => $item) {
+                if (is_array($item) == false) {
+                    $item = json_decode($item, true);
+                }
+
+                $this->on_data[$index] = new self($item);
             }
-            return $this;
+
+            return;
+        }
+
+        if (is_array($attributes) == false) {
+            $attributes = json_decode($attributes, true);
         }
 
         $this->flag_emoji = $attributes;
-        return $this;
+        return;
     }
 
     /**
-     * Return the flag emoji attributes as an array.
+     * Return the flag emoji attribute.
      *
-     * If the class was constructed with a collection of items, this method will
-     * return a collection of the flag emoji attributes for each item.
+     * If `$flagKey` is not set, this method will return a Collection of all flag emoji
+     * attributes. If `$flagKey` is set, this method will return the flag emoji
+     * attribute at the given key.
      *
-     * @return array|\Illuminate\Support\Collection
+     * @param string|int $flagKey The key of the flag emoji attribute to return.
+     * @return Collection|FlagEmoji A Collection of all flag emoji attributes, or the flag emoji
+     *                              attribute at the given key.
      */
-    public function get()
+    public function get(string|int $flagKey = null): Collection|FlagEmoji
     {
-        if ($this->on_data != null) {
-            return collect($this->on_data);
+        if ($flagKey !== null) {
+           return $this->on_data[$flagKey];
         }
 
-        return $this->flag_emoji;
+        return collect($this->on_data);
     }
 
     /**
@@ -57,10 +71,11 @@ final class FlagEmoji
      *
      * @return Emoji|\Illuminate\Support\Collection
      */
-    public function first()
+    public function first(): ?FlagEmoji
     {
         $this->flag_emoji = collect($this->on_data)->first();
-        return $this;
+        $this->key = collect($this->on_data)->keys()->first();
+        return $this->flag_emoji;
     }
 
     /**
@@ -73,10 +88,42 @@ final class FlagEmoji
      *
      * @return Emoji|\Illuminate\Support\Collection
      */
-    public function last()
+    public function last(): ?FlagEmoji
     {
-        $this->flag_emoji = collect($this->on_data)->last();
-        return $this;
+        $this->flag_emoji = Arr::last($this->on_data);
+        $this->key = collect($this->on_data)->keys()->last();
+        return $this->flag_emoji;
+    }
+
+    /**
+     * Return the key of the flag emoji attribute.
+     *
+     * If the class was constructed with a collection of items, this method will
+     * return a collection of the keys from the collection. If the class was
+     * constructed with a string, this method will return the key of the flag emoji
+     * attribute as a string.
+     *
+     * @return string|\Illuminate\Support\Collection
+     */
+    public function keys()
+    {
+        return collect($this->on_data)->keys();
+    }
+
+    /**
+     * Return the key of the flag emoji attribute.
+     *
+     * If the class was constructed with a collection of items, this method will
+     * return a collection of the keys from the collection. If the class was
+     * constructed with a string, this method will return the key of the flag emoji
+     * attribute as a string.
+     *
+     * @return string|\Illuminate\Support\Collection
+     */
+    public function key()
+    {
+        $this->first();
+        return $this->key;
     }
 
 }
--- src/models/Concerns/HasCountriesList.php
+++ src/models/Concerns/HasCountriesList.php
@@ -3,6 +3,7 @@
 namespace Lwwcas\LaravelCountries\Models\Concerns;
 
 use Illuminate\Support\Facades\Cache;
+use Lwwcas\LaravelCountries\Facades\FlagEmoji;
 use Lwwcas\LaravelCountries\Models\Country;
 
 trait HasCountriesList
@@ -28,12 +29,12 @@ trait HasCountriesList
     public function withNamesAndSlugs()
     {
         return Country::select(
-            'lc_countries.id',
-            'lc_countries.uid',
-            'lc_countries.official_name',
-            'lc_countries.iso_alpha_2',
-            'lc_countries.iso_alpha_3',
-            'lc_countries_translations.name as name'
+            'lc_countries.id as id',
+            'lc_countries.uid as uid',
+            'lc_countries.official_name as official_name',
+            'lc_countries.iso_alpha_2 as iso_alpha_2',
+            'lc_countries.iso_alpha_3 as iso_alpha_3',
+            'lc_countries_translations.name as name',
         )
             ->join(
                 'lc_countries_translations',
@@ -58,13 +59,13 @@ trait HasCountriesList
     public function withNamesSlugsAndFlags()
     {
         return Country::select(
-            'lc_countries.id',
-            'lc_countries.uid',
-            'lc_countries.official_name',
-            'lc_countries.iso_alpha_2',
-            'lc_countries.iso_alpha_3',
-            'lc_countries.flag_emoji',
-            'lc_countries_translations.name as name'
+            'lc_countries.id as id',
+            'lc_countries.uid as uid',
+            'lc_countries.official_name as official_name',
+            'lc_countries.iso_alpha_2 as iso_alpha_2',
+            'lc_countries.iso_alpha_3 as iso_alpha_3',
+            'lc_countries.flag_emoji as flag_emoji',
+            'lc_countries_translations.name as name',
         )
             ->join(
                 'lc_countries_translations',
@@ -76,6 +77,381 @@ trait HasCountriesList
                 $query->select('lc_country_id', 'name', 'slug');
             }])
             ->orderBy('name', 'asc');
+    }
+
+    /**
+     * Get a list of country IDs.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function onlyId()
+    {
+        return $this->withNamesAndSlugs()->pluck('id');
+    }
+
+    /**
+     * Get a list of country UIDs.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function onlyUid()
+    {
+        return $this->withNamesAndSlugs()->pluck('uid');
+    }
+
+    /**
+     * Get a list of country names.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function onlyName()
+    {
+        return $this->withNamesAndSlugs()->pluck('name');
+    }
+
+    /**
+     * Get a list of official country names.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function onlyOfficialName()
+    {
+        return $this->withNamesAndSlugs()->pluck('official_name');
+    }
+
+    /**
+     * Get a list of ISO 3166-1 alpha-2 codes.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function onlyIso()
+    {
+        return $this->onlyAlpha2();
+    }
+
+    /**
+     * Get a list of ISO 3166-1 alpha-2 codes.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function onlyAlpha2()
+    {
+        return $this->withNamesAndSlugs()->pluck('iso_alpha_2');
+    }
+
+    /**
+     * Get a list of ISO 3166-1 alpha-3 codes.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function onlyAlpha3()
+    {
+        return $this->withNamesAndSlugs()->pluck('iso_alpha_3');
+    }
+
+
+    /**
+     * Get a list of flag emojis.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function onlyFlag()
+    {
+        return $this->onlyEmoji();
+    }
+
+    /**
+     * Get a list of country flag emojis.
+     *
+     * This method return a list of country flag emojis.
+     * The list is cached for a long time to avoid to query the database too much.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function onlyEmoji()
+    {
+        $result =$this->withNamesSlugsAndFlags()->pluck('flag_emoji') ?? collect([]);
+        return (new FlagEmoji($result));
+    }
+
+    /**
+     * Get a list of countries with their ids and official names.
+     *
+     * This method returns a list of countries with their ids and official names.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function idAndOfficialName()
+    {
+        return $this->withNamesAndSlugs()->pluck('official_name', 'id');
+    }
+
+    /**
+     * Get a list of countries with their ids and UIDs.
+     *
+     * This method returns a list of countries with their ids and UIDs.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function idAndUid()
+    {
+        return $this->withNamesAndSlugs()->pluck('uid', 'id');
+    }
+
+    /**
+     * Get a list of countries with their ids and names.
+     *
+     * This method returns a list of countries with their ids and names.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function idAndName()
+    {
+        return $this->withNamesAndSlugs()->pluck('name', 'id');
+    }
+
+    /**
+     * Get a list of countries with their ids and iso_alpha_2 codes.
+     *
+     * This method returns a list of countries with their ids and iso_alpha_2 codes.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function idAndAlpha2()
+    {
+        return $this->withNamesAndSlugs()->pluck('iso_alpha_2', 'id');
+    }
+
+    /**
+     * Get a list of countries with their ids and iso_alpha_3 codes.
+     *
+     * This method returns a list of countries with their ids and iso_alpha_3 codes.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function idAndAlpha3()
+    {
+        return $this->withNamesAndSlugs()->pluck('iso_alpha_3', 'id');
+    }
+
+    /**
+     * Get a list of countries with their ids and emojis.
+     *
+     * This method returns a list of countries with their ids and emojis.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function idAndEmoji()
+    {
+        $result = $this->withNamesSlugsAndFlags()->pluck('flag_emoji', 'id') ?? collect([]);
+        return (new FlagEmoji($result));
+    }
+
+    /**
+     * Get a list of countries with their uids and names.
+     *
+     * This method returns a list of countries with their uids and names.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function uidAndName()
+    {
+        return $this->withNamesAndSlugs()->pluck('name', 'uid');
+    }
+
+    /**
+     * Get a list of countries with their uids and official names.
+     *
+     * This method returns a list of countries with their uids and official names.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function uidAndOfficialName()
+    {
+        return $this->withNamesAndSlugs()->pluck('official_name', 'uid');
+    }
+
+    /**
+     * Get a list of countries with their uids and iso_alpha_2 codes.
+     *
+     * This method returns a list of countries with their uids and iso_alpha_2 codes.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function uidAndAlpha2()
+    {
+        return $this->withNamesAndSlugs()->pluck('iso_alpha_2', 'uid');
+    }
+
+    /**
+     * Get a list of countries with their uids and iso_alpha_3 codes.
+     *
+     * This method returns a list of countries with their uids and iso_alpha_3 codes.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function uidAndAlpha3()
+    {
+        return $this->withNamesAndSlugs()->pluck('iso_alpha_3', 'uid');
+    }
+
+    /**
+     * Get a list of countries with their uids and emojis.
+     *
+     * This method returns a list of countries with their uids and emojis.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function uidAndEmoji()
+    {
+        $result = $this->withNamesSlugsAndFlags()->pluck('flag_emoji', 'uid') ?? collect([]);
+        return (new FlagEmoji($result));
+    }
+
+    /**
+     * Get a list of countries with their names and official names.
+     *
+     * This method returns a list of countries with their names and official names.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function nameAndOfficialName()
+    {
+        return $this->withNamesAndSlugs()->pluck('official_name', 'name');
+    }
+
+    /**
+     * Get a list of countries with their names and iso_alpha_2 codes.
+     *
+     * This method returns a list of countries with their names and iso_alpha_2 codes.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function nameAndAlpha2()
+    {
+        return $this->withNamesAndSlugs()->pluck('iso_alpha_2', 'name');
+    }
+
+    /**
+     * Get a list of countries with their names and iso_alpha_3 codes.
+     *
+     * This method returns a list of countries with their names and iso_alpha_3 codes.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function nameAndAlpha3()
+    {
+        return $this->withNamesAndSlugs()->pluck('iso_alpha_3', 'name');
+    }
+
+    /**
+     * Get a list of countries with their names and emojis.
+     *
+     * This method returns a list of countries with their names and emojis.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function nameAndEmoji()
+    {
+        $result = $this->withNamesSlugsAndFlags()->pluck('flag_emoji', 'name') ?? collect([]);
+        return (new FlagEmoji($result));
+    }
+
+    /**
+     * Get a list of countries with their official names and iso_alpha_2 codes.
+     *
+     * This method returns a list of countries with their official names and iso_alpha_2 codes.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function officialNameAndAlpha2()
+    {
+        return $this->withNamesAndSlugs()->pluck('iso_alpha_2', 'official_name');
+    }
+
+    /**
+     * Get a list of countries with their official names and iso_alpha_3 codes.
+     *
+     * This method returns a list of countries with their official names and iso_alpha_3 codes.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function officialNameAndAlpha3()
+    {
+        return $this->withNamesAndSlugs()->pluck('iso_alpha_3', 'official_name');
+    }
+
+    /**
+     * Get a list of countries with their official names and emojis.
+     *
+     * This method returns a list of countries with their official names and emojis.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function officialNameAndEmoji()
+    {
+        $result = $this->withNamesSlugsAndFlags()->pluck('flag_emoji', 'official_name') ?? collect([]);
+        return (new FlagEmoji($result));
+    }
+
+    /**
+     * Get a list of countries with their iso_alpha_2 and iso_alpha_3 codes.
+     *
+     * This method returns a list of countries with their iso_alpha_2 and iso_alpha_3 codes.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function alpha2AndAlpha3()
+    {
+        return $this->withNamesAndSlugs()->pluck('iso_alpha_3', 'iso_alpha_2');
+    }
+
+    /**
+     * Get a list of countries with their iso_alpha_2 codes and emojis.
+     *
+     * This method returns a list of countries with their iso_alpha_2 codes and emojis.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function alpha2AndEmoji()
+    {
+        $result = $this->withNamesSlugsAndFlags()->pluck('flag_emoji', 'iso_alpha_2') ?? collect([]);
+        return (new FlagEmoji($result));
+    }
+
+    /**
+     * Get a list of countries with their iso_alpha_3 codes and emojis.
+     *
+     * This method returns a list of countries with their iso_alpha_3 codes and emojis.
+     * The list is cached for a long time to avoid querying the database too much.
+     *
+     * @return Collection
+     */
+    public function alpha3AndEmoji()
+    {
+        $result = $this->withNamesSlugsAndFlags()->pluck('flag_emoji', 'iso_alpha_3') ?? collect([]);
+        return (new FlagEmoji($result));
+
     }
 
 }
