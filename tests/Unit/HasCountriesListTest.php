25cbead416d53dd73b051bf0f8e92e4b5b7dc918
Lucas Monteiro <lwwcas@gmail.com>
2026-07-13 23:17:30 +0100

    test: assert country list methods return all records

--- tests/Unit/HasCountriesListTest.php
+++ tests/Unit/HasCountriesListTest.php
@@ -1,99 +1,157 @@
 <?php
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
 use Lwwcas\LaravelCountries\Database\Factories\CountryFactory;
 use Lwwcas\LaravelCountries\Facades\FlagEmoji;
 use Lwwcas\LaravelCountries\Models\Country;
 
-it('should return a list of countries with name and slugs efficiently', function () {
-    foreach (range(1, 30) as $item) {
+const COUNTRY_LIST_COUNT = 15;
+
+function seedCountriesForList(int $count = COUNTRY_LIST_COUNT): void
+{
+    foreach (range(1, $count) as $item) {
         CountryFactory::new()->create([
             'en' => [
                 'name' => fake()->country() . ' ' . rand(3, 9999),
-            ]
+            ],
         ]);
     }
+}
 
-    $counties = Country::getList()->withNamesSlugsAndFlags()->get();
+it('returns all countries from list query builder methods', function (string $method) {
+    seedCountriesForList();
 
-    expect($counties)->each->toBeInstanceOf(Country::class);
-    expect($counties->count())->toBe(30);
-    expect($counties->first())->toHaveKey('iso_alpha_2');
-    expect($counties->first())->toHaveKey('iso_alpha_3');
-});
+    $result = Country::getList()->{$method}();
 
-it('should return a list of countries with name and slugs and flags efficiently', function () {
-    foreach (range(1, 30) as $item) {
-        CountryFactory::new()->create([
-            'en' => [
-                'name' => fake()->country() . ' ' . rand(3, 9999),
-            ]
-        ]);
-    }
+    expect($result)->toBeInstanceOf(Builder::class);
+    expect($result->get())->toHaveCount(COUNTRY_LIST_COUNT);
+})->with([
+    'withNamesAndSlugs',
+    'withNamesSlugsAndFlags',
+]);
 
-    $counties = Country::getList()->withNamesSlugsAndFlags()->get();
-    $emoji = $counties->first()->flag_emoji;
+it('returns all countries from only list methods', function (string $method) {
+    seedCountriesForList();
 
-    expect($counties)->each->toBeInstanceOf(Country::class);
-    expect($counties->first())->toHaveKey('iso_alpha_2');
-    expect($counties->first())->toHaveKey('iso_alpha_3');
-    expect($counties->first())->toHaveKey('flag_emoji');
-    expect($counties->count())->toBe(30);
-    expect($emoji)->toBeArray();
-    expect($emoji)->toHaveKey('img');
+    $result = Country::getList()->{$method}();
 
-});
+    expect($result)->toBeInstanceOf(Collection::class);
+    expect($result)->toHaveCount(COUNTRY_LIST_COUNT);
+})->with([
+    'onlyId',
+    'onlyUid',
+    'onlyName',
+    'onlyOfficialName',
+    'onlyIso',
+    'onlyAlpha2',
+    'onlyAlpha3',
+    'onlyEmojiInImg',
+    'onlyEmojiInUtf8',
+    'onlyEmojiInUtf16',
+    'onlyEmojiInHtml',
+    'onlyEmojiInCss',
+    'onlyEmojiInHex',
+    'onlyEmojiInUCode',
+    'onlyEmojiInDecimal',
+    'onlyEmojiInShortCode',
+]);
 
-it('should return methods returning expected results', function ($method, $keyType, $valueType, $keyName, $valueName) {
-    foreach (range(1, 30) as $item) {
-        CountryFactory::new()->create([
-            'en' => [
-                'name' => fake()->country() . ' ' . rand(3, 9999),
-                ]
-            ]);
-        }
+it('returns all countries from only emoji list methods', function (string $method) {
+    seedCountriesForList();
+
+    $result = Country::getList()->{$method}();
+
+    expect($result)->toBeInstanceOf(FlagEmoji::class);
+    expect($result->get())->toHaveCount(COUNTRY_LIST_COUNT);
+})->with([
+    'onlyFlag',
+    'onlyEmoji',
+]);
+
+it('returns all countries from pair list methods', function (string $method) {
+    seedCountriesForList();
+
+    $result = Country::getList()->{$method}();
+
+    expect($result)->toBeInstanceOf(Collection::class);
+    expect($result)->toHaveCount(COUNTRY_LIST_COUNT);
+})->with([
+    'idAndOfficialName',
+    'idAndUid',
+    'idAndName',
+    'idAndAlpha2',
+    'idAndAlpha3',
+    'uidAndName',
+    'uidAndOfficialName',
+    'uidAndAlpha2',
+    'uidAndAlpha3',
+    'nameAndOfficialName',
+    'nameAndAlpha2',
+    'nameAndAlpha3',
+    'officialNameAndAlpha2',
+    'officialNameAndAlpha3',
+    'alpha2AndId',
+    'alpha2AndUid',
+    'alpha2AndName',
+    'alpha2AndOfficialName',
+    'alpha2AndAlpha3',
+    'alpha3AndId',
+    'alpha3AndUid',
+    'alpha3AndName',
+    'alpha3AndOfficialName',
+]);
+
+it('returns all countries from pair emoji list methods', function (string $method) {
+    seedCountriesForList();
+
+    $result = Country::getList()->{$method}();
+
+    expect($result)->toBeInstanceOf(FlagEmoji::class);
+    expect($result->get())->toHaveCount(COUNTRY_LIST_COUNT);
+})->with([
+    'idAndEmoji',
+    'uidAndEmoji',
+    'nameAndEmoji',
+    'officialNameAndEmoji',
+    'alpha2AndEmoji',
+    'alpha3AndEmoji',
+]);
+
+it('should return methods returning expected results', function ($method, $keyType, $valueType) {
+    seedCountriesForList(COUNTRY_LIST_COUNT);
 
     $list = Country::getList()->{$method}();
 
     expect($list)->toBeInstanceOf(Collection::class);
-    expect($list)->not->toBeEmpty();
+    expect($list)->toHaveCount(COUNTRY_LIST_COUNT);
     expect($list->keys()->first())->{$keyType}();
     expect($list->first())->{$valueType}();
 })->with([
-    ['method' => 'idAndUid', 'keyType' => 'toBeInt', 'valueType' => 'toBeString', 'keyName' => 'id', 'valueName' => 'uid'],
-    ['method' => 'idAndName', 'keyType' => 'toBeInt', 'valueType' => 'toBeString', 'keyName' => 'id', 'valueName' => 'name'],
-    ['method' => 'idAndOfficialName', 'keyType' => 'toBeInt', 'valueType' => 'toBeString', 'keyName' => 'id', 'valueName' => 'official_name'],
-    ['method' => 'idAndAlpha2', 'keyType' => 'toBeInt', 'valueType' => 'toBeString', 'keyName' => 'id', 'valueName' => 'alpha_2'],
-    ['method' => 'idAndAlpha3', 'keyType' => 'toBeInt', 'valueType' => 'toBeString', 'keyName' => 'id', 'valueName' => 'alpha_3'],
-
-    ['method' => 'uidAndName', 'keyType' => 'toBeString', 'valueType' => 'toBeString', 'keyName' => 'uid', 'valueName' => 'name'],
-    ['method' => 'uidAndOfficialName', 'keyType' => 'toBeString', 'valueType' => 'toBeString', 'keyName' => 'uid', 'valueName' => 'official_name'],
-    ['method' => 'uidAndAlpha2', 'keyType' => 'toBeString', 'valueType' => 'toBeString', 'keyName' => 'uid', 'valueName' => 'alpha_2'],
-    ['method' => 'uidAndAlpha3', 'keyType' => 'toBeString', 'valueType' => 'toBeString', 'keyName' => 'uid', 'valueName' => 'alpha_3'],
-
-    ['method' => 'nameAndOfficialName', 'keyType' => 'toBeString', 'valueType' => 'toBeString', 'keyName' => 'name', 'valueName' => 'official_name'],
-    ['method' => 'nameAndAlpha2', 'keyType' => 'toBeString', 'valueType' => 'toBeString', 'keyName' => 'name', 'valueName' => 'alpha_2'],
-    ['method' => 'nameAndAlpha3', 'keyType' => 'toBeString', 'valueType' => 'toBeString', 'keyName' => 'name', 'valueName' => 'alpha_3'],
-
-    ['method' => 'officialNameAndAlpha2', 'keyType' => 'toBeString', 'valueType' => 'toBeString', 'keyName' => 'official_name', 'valueName' => 'alpha_2'],
-    ['method' => 'officialNameAndAlpha3', 'keyType' => 'toBeString', 'valueType' => 'toBeString', 'keyName' => 'official_name', 'valueName' => 'alpha_3'],
-
-    ['method' => 'alpha2AndAlpha3', 'keyType' => 'toBeString', 'valueType' => 'toBeString', 'keyName' => 'alpha_2', 'valueName' => 'alpha_3'],
+    ['method' => 'idAndUid', 'keyType' => 'toBeInt', 'valueType' => 'toBeString'],
+    ['method' => 'idAndName', 'keyType' => 'toBeInt', 'valueType' => 'toBeString'],
+    ['method' => 'idAndOfficialName', 'keyType' => 'toBeInt', 'valueType' => 'toBeString'],
+    ['method' => 'idAndAlpha2', 'keyType' => 'toBeInt', 'valueType' => 'toBeString'],
+    ['method' => 'idAndAlpha3', 'keyType' => 'toBeInt', 'valueType' => 'toBeString'],
+    ['method' => 'uidAndName', 'keyType' => 'toBeString', 'valueType' => 'toBeString'],
+    ['method' => 'uidAndOfficialName', 'keyType' => 'toBeString', 'valueType' => 'toBeString'],
+    ['method' => 'uidAndAlpha2', 'keyType' => 'toBeString', 'valueType' => 'toBeString'],
+    ['method' => 'uidAndAlpha3', 'keyType' => 'toBeString', 'valueType' => 'toBeString'],
+    ['method' => 'nameAndOfficialName', 'keyType' => 'toBeString', 'valueType' => 'toBeString'],
+    ['method' => 'nameAndAlpha2', 'keyType' => 'toBeString', 'valueType' => 'toBeString'],
+    ['method' => 'nameAndAlpha3', 'keyType' => 'toBeString', 'valueType' => 'toBeString'],
+    ['method' => 'officialNameAndAlpha2', 'keyType' => 'toBeString', 'valueType' => 'toBeString'],
+    ['method' => 'officialNameAndAlpha3', 'keyType' => 'toBeString', 'valueType' => 'toBeString'],
+    ['method' => 'alpha2AndAlpha3', 'keyType' => 'toBeString', 'valueType' => 'toBeString'],
 ]);
 
 it('should return emoji methods returning expected results', function ($method, $keyType) {
-    foreach (range(1, 30) as $item) {
-        CountryFactory::new()->create([
-            'en' => [
-                'name' => fake()->country() . ' ' . rand(3, 9999),
-            ]
-        ]);
-    }
+    seedCountriesForList(COUNTRY_LIST_COUNT);
 
-    $list = Country::getList()->alpha3AndEmoji();
     $list = Country::getList()->{$method}();
 
-    expect($list)->not->toBeEmpty();
+    expect($list)->toBeInstanceOf(FlagEmoji::class);
+    expect($list->get())->toHaveCount(COUNTRY_LIST_COUNT);
     expect($list->keys()->first())->{$keyType}();
     expect($list->first())->toBeInstanceOf(FlagEmoji::class);
 })->with([
